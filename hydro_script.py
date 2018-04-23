d8bb8cb87fe7c027b4fef34f1d9bf5a315b48975
mchiapino <mchiapino@gmail.com>
2018-04-23 10:35:50 +0200

    clean

--- hydro_script.py
+++ hydro_script.py
@@ -16,16 +16,21 @@ p_k = 0.05
 R = np.max(x_rank,
            axis=1)[np.argsort(np.max(x_rank, axis=1))[::-1][int(n*p_k)]]
 
-# p_k = 0.005
-# k = int(n*p_k)
-# x_bin_k = extr.extreme_points_bin(x_rank, k)
-# x_bin_kp = extr.extreme_points_bin(x_rank, k + int(k**(3./4)))
-# x_bin_km = extr.extreme_points_bin(x_rank, k - int(k**(3./4)))
-# n_extr = np.sum(np.sum(x_bin_k, axis=1) > 0)
+p_k = 0.005
+k = int(n*p_k)
+x_bin_k = extr.extreme_points_bin(x_rank, k)
+x_bin_kp = extr.extreme_points_bin(x_rank, k + int(k**(3./4)))
+x_bin_km = extr.extreme_points_bin(x_rank, k - int(k**(3./4)))
+n_extr = np.sum(np.sum(x_bin_k, axis=1) > 0)
+
+# Hill (0.01, 0.01) (0.02, 0.0005)
+delta = 0.05
+alphas_hill = hill.hill_0(x_rank, x_bin_k, x_bin_kp, x_bin_km, delta, k)
+hm.map_visualisation(alphas_hill, d)
 
 # # Clef (0.01, 0.2)
-x_bin_clf = 1.*(x_rank > R)
-x_bin_clf = x_bin_clf[np.sum(x_bin_clf, axis=1) > 1]
+# x_bin_clf = 1.*(x_rank > R)
+# x_bin_clf = x_bin_clf[np.sum(x_bin_clf, axis=1) > 1]
 # kappa_min = 0.2
 # alphas_clf = clf.clef_0(x_bin_clf, kappa_min)
 # hm.map_visualisation(alphas_clf, d)
@@ -44,22 +49,3 @@ x_bin_clf = x_bin_clf[np.sum(x_bin_clf, axis=1) > 1]
 # alphas_dmx, mass = dmx.damex_0(x_bin_dmx)
 # alphas_dmx = alphas_dmx[:np.sum(mass > 1)]
 # hm.map_visualisation(alphas_dmx, d)
-
-# # Hill (0.01, 0.01) (0.02, 0.0005)
-# delta = 0.01
-
-# # Peng (0.01, 0.001, 0.4)
-# delta = 0.01
-
-# # r threshold (0.01, 0.4)
-# r_thresh = 0.4
-
-# # Damex (0.01, 0.5)
-# eps = 0.5
-# R = n_samples/(k + 1.)
-
-# # Kappa (0.01, 0.005, 0.4)
-# delta = 0.005
-# kappa_min = 0.4
-
-# Visualization
--- logistic_visualization.py
+++ logistic_visualization.py
@@ -4,20 +4,61 @@ from mpl_toolkits.mplot3d import Axes3D
 
 import logistic as lg
 
-alphas = [[0, 1], [2]]
+alphas = [[2], [0, 1]]
 d = 3
-n = int(1e5)
-as_dep = 0.3
+n = int(2e3)
+as_dep = 0.1
 X_lgtc = lg.asym_logistic(d, alphas, n, as_dep)
+R_lgtc = np.sum(X_lgtc, axis=1)
+ind_bound = R_lgtc < np.percentile(R_lgtc, 99.5)
+X_lgtc = X_lgtc[ind_bound]
+R_lgtc = R_lgtc[ind_bound]
+W_lgtc = (X_lgtc.T / R_lgtc).T
+pctile = 95
+ind_extr = R_lgtc > np.percentile(R_lgtc, pctile)
+W_lg_extr = W_lgtc[ind_extr]
+
 
 # Visu #
+xe = X_lgtc[ind_extr, 0]
+ye = X_lgtc[ind_extr, 1]
+ze = X_lgtc[ind_extr, 2]
 xs = X_lgtc[:, 0]
 ys = X_lgtc[:, 1]
 zs = X_lgtc[:, 2]
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
+ax.set_axis_off()
 ax.scatter(xs, ys, zs)
-# ax.plot(np.array([0, np.max(X_lgtc)]), np.zeros(2), np.zeros(2), 'k')
-# ax.plot(np.zeros(2), np.array([0, np.max(X_lgtc)]), np.zeros(2), 'k')
-# ax.plot(np.zeros(2), np.zeros(2), np.array([0, np.max(X_lgtc)]), 'k')
+ax.scatter(xe, ye, ze, c='r')
+X = np.array([np.percentile(R_lgtc, pctile), 0, 0])
+Y = np.array([0, np.percentile(R_lgtc, pctile), 0])
+Z = np.array([0, 0, np.percentile(R_lgtc, pctile)])
+ax.plot_trisurf(X, Y, Z, color='r', alpha=0.2)
+ax.plot([0, np.max(X_lgtc)], [0, 0], [0, 0], 'k')
+ax.plot([0, 0], [0, np.max(X_lgtc)], [0, 0], 'k')
+ax.plot([0, 0], [0, 0], [0, np.max(X_lgtc)], 'k')
+plt.show()
+
+
+# Angle Visu
+xw = W_lgtc[:, 0]
+yw = W_lgtc[:, 1]
+zw = W_lgtc[:, 2]
+xw_e = W_lg_extr[:, 0]
+yw_e = W_lg_extr[:, 1]
+zw_e = W_lg_extr[:, 2]
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.set_axis_off()
+# ax.scatter(xw, yw, zw)
+# , c=c, cmap='viridis'
+ax.scatter(xw_e, yw_e, zw_e, c='r')
+X = np.array([1, 0, 0])
+Y = np.array([0, 1, 0])
+Z = np.array([0, 0, 1])
+ax.plot_trisurf(X, Y, Z, color='b', alpha=0.2)
+ax.plot([0, 1], [0, 0], [0, 0], 'k')
+ax.plot([0, 0], [0, 1], [0, 0], 'k')
+ax.plot([0, 0], [0, 0], [0, 1], 'k')
 plt.show()
